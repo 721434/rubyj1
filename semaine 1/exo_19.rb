aa7d7794971c0fa260e9ba2134442a50401f05b3
721434 <viguiegw@laposte.net>
2019-07-09 20:05:29 +0200

    vrai

--- semaine 1/exo_19.rb
+++ semaine 1/exo_19.rb
@@ -1,54 +1,12 @@
 email = []
 
 50.times do |i|
-email.push ("paul#{i + 1}.dupont@gmail.com")
 
-email.each do |i|
-    if i % 2 == 0
-puts i
-    end
+
+    if (i % 2).round == 0
+        email.push ("paul#{i + 2}.dupont@gmail.com")
+        puts email[i]
+    else
+        email.push ("")
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]
 end
